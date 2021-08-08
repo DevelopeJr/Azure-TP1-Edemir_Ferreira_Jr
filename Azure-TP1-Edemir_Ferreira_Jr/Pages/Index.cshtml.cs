94d7ee672c1b7d319546de5be29ae5bf6e5aad97
DevelopeJr <edemir.ferreira.jr@gmail.com>
2021-08-07 21:09:08 -0300

    Add project files.

---
+++ Azure-TP1-Edemir_Ferreira_Jr/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure_TP1_Edemir_Ferreira_Jr.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
